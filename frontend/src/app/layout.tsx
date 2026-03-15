9d5f0cb750235b3f1a7583b09ec069913ef2068a
Jo Asano Mac <69098381+Jo042@users.noreply.github.com>
2026-03-15 21:16:21 +0900

    feat(front) ApolloProvider設定:

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./provider";
 
 // 日本語フォントの設定
 const notoSansJP = Noto_Sans_JP({
   variable: "--font-noto-sans-jp",
   subsets: ["latin"],
-  weight: ["400", "500", "700"], 
+  weight: ["400", "500", "700"],
   display: "swap",
 });
 
 export const metadata: Metadata = {
   title: {
     default: "Spotee | スポット共有サービス",
-    template: "%s | Spotee",  // 各ページで title を設定すると "ページ名 | Spotee" になる
+    template: "%s | Spotee", // 各ページで title を設定すると "ページ名 | Spotee" になる
   },
   description: "すべての人が、次の行きたい場所に出会えるサービス",
 };
@@ -26,8 +27,8 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${notoSansJP.variable} font-sans antialiased`}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
