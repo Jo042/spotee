cbb2a166eeb8747b85955e90aa90931e55dd3fe7
Jo Asano Mac <69098381+Jo042@users.noreply.github.com>
2026-03-23 13:48:03 +0900

    feat(backend): SpotModule の作成（CRUD）

--- backend/src/spot/spot.module.ts
+++ backend/src/spot/spot.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SpotService } from './spot.service';
 import { SpotResolver } from './spot.resolver';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  providers: [SpotService, SpotResolver],
+  imports: [UserModule],
+  providers: [SpotResolver, SpotService],
+  exports: [SpotService],
 })
 export class SpotModule {}
--- backend/src/spot/spot.resolver.ts
+++ backend/src/spot/spot.resolver.ts
@@ -1,4 +1,56 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { SpotService } from './spot.service';
+import { UserService } from '../user/user.service';
+import { Spot } from './dto/spot.object';
+import { CreateSpotInput } from './dto/create-spot.input';
+import { UpdateSpotInput } from './dto/update-spot.input';
+import { GqlAuthGuard } from '../auth/auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import type { AuthUser } from 'src/auth/types/auth-user.type';
 
-@Resolver()
-export class SpotResolver {}
+@Resolver(() => Spot)
+export class SpotResolver {
+  constructor(
+    private spotService: SpotService,
+    private userService: UserService,
+  ) {}
+
+  @Mutation(() => Spot)
+  @UseGuards(GqlAuthGuard)
+  async createSpot(
+    @CurrentUser() authUser: AuthUser,
+    @Args('input') input: CreateSpotInput,
+  ): Promise<Spot> {
+    const user = await this.userService.getOrCreateUser(authUser);
+    return this.spotService.create(user.id, input);
+  }
+
+  @Query(() => Spot, { name: 'spot', nullable: true })
+  async getSpot(
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<Spot | null> {
+    return this.spotService.findById(id);
+  }
+
+  @Mutation(() => Spot)
+  @UseGuards(GqlAuthGuard)
+  async updateSpot(
+    @CurrentUser() authUser: AuthUser,
+    @Args('id', { type: () => ID }) id: string,
+    @Args('input') input: UpdateSpotInput,
+  ): Promise<Spot> {
+    const user = await this.userService.getOrCreateUser(authUser);
+    return this.spotService.update(user.id, id, input);
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(GqlAuthGuard)
+  async deleteSpot(
+    @CurrentUser() authUser: AuthUser,
+    @Args('id', { type: () => ID }) id: string,
+  ): Promise<boolean> {
+    const user = await this.userService.getOrCreateUser(authUser);
+    return this.spotService.delete(user.id, id);
+  }
+}
